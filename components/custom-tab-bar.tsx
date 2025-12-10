bfabd257344c3f4c39592851ce6260d6aa97fc07
banexxl <damjanovic.branislav@gmail.com>
2025-12-10 21:18:21 +0100

    feat: reduce height of custom tab bar from 120 to 70 for improved layout

--- components/custom-tab-bar.tsx
+++ components/custom-tab-bar.tsx
@@ -257,7 +257,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.1,
     shadowRadius: 4,
     elevation: 8,
-    height: 120,
+    height: 70,
     position: 'absolute',
     left: 0,
     right: 0,
